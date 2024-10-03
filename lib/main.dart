ae2efe6258239091de7a85dfab1e443592735722
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-10-02 21:25:40 -0400

    inicializacion para poder cambiar idioma de calendario y otros

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:diary_app/config/router/app_router.dart';
 import 'package:diary_app/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() => runApp(const DiaryApp());
+Future<void> main() async {
+  await initializeDateFormatting('es', null);
+  runApp(const DiaryApp());
+}
 
 class DiaryApp extends StatelessWidget {
   const DiaryApp({super.key});
