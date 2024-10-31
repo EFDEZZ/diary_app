34f42670a2d1f763147c55bf4ec28cd90df080f3
EDRIEN FDEZ <edriennn2001@gmail.com>
2024-10-31 13:29:53 -0400

    Creando la entidad para los datos de la HCE

---
+++ lib/domain/entities/electronic_medical_record.dart
@@ -0,0 +1,25 @@
+class ElectronicMedicalRecord {
+  final int patientId;
+  final String patientName;
+  final DateTime dateOfBirth;
+  final String gender;
+  final String chronicDiseases;
+  final String allergies;
+  final String currentMedications;
+  final String consultations;
+  final String labResults;
+  final String hospitalizations;
+
+  ElectronicMedicalRecord({
+    required this.patientId,
+    required this.patientName,
+    required this.dateOfBirth,
+    required this.gender,
+    required this.chronicDiseases,
+    required this.allergies,
+    required this.currentMedications,
+    required this.consultations,
+    required this.labResults,
+    required this.hospitalizations
+  });
+}
